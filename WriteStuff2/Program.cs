c9a19f558a7365e0641638722560d4f2cc64705f
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-06-02 14:58:07 +0100

    Exercise 7
    Added class Program from WriteStuff1

--- WriteStuff2/Program.cs
+++ WriteStuff2/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
 
-namespace WriteStuff2
+namespace WriteStuff1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Queue<string> writeLines = new Queue<string>();
+
+            string input = Console.ReadLine();
+            while (input != "")
+            {
+                writeLines.Enqueue(input);
+                input = Console.ReadLine();
+            }
+
+                foreach (string s in writeLines)
+            {
+                File.WriteAllLines(args[0], writeLines);
+                }
+            }
         }
     }
-}
