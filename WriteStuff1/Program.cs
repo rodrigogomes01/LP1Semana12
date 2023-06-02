9891c1a83409bdc1af800e1eb0ffeab59c77fac0
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-06-02 14:49:53 +0100

    Exercise 6
    Added Queue<string> to the project

--- WriteStuff1/Program.cs
+++ WriteStuff1/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
 
 namespace WriteStuff1
 {
@@ -6,7 +10,22 @@ namespace WriteStuff1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Queue<string> writeLines = new Queue<string>();
+
+            using (StreamWriter sw = File.CreateText(args[0]))
+            {
+                string input = Console.ReadLine();
+                while (input != "")
+                {
+                    writeLines.Enqueue(input);
+                    input = Console.ReadLine();
+                }
+
+                foreach (string s in writeLines)
+                {
+                    sw.WriteLine(s);
+                }
+            }
         }
     }
 }
