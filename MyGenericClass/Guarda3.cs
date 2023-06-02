57f19ad3b1ff360ecc858b634c5d1cc8ef43369a
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-06-02 13:56:58 +0100

    Exercise 3
    Created methods SetItem and GetItem

--- MyGenericClass/Guarda3.cs
+++ MyGenericClass/Guarda3.cs
@@ -7,6 +7,33 @@ namespace MyGenericClass
 {
     public class Guarda3<T>
     {
+        private List<T> variableList = new List<T>();
+        private T v1, v2, v3;
+
+        public Guarda3()
+        {
+            v1 = default;
+            v2 = default;
+            v3 = default;
+
+            variableList.Add(v1);
+            variableList.Add(v2);
+            variableList.Add(v3);
+        }
+
+        public T GetItem(int i)
+        {
+            return variableList[i];
+        }
+        public void SetItem(int i, T item)
+        {
+
+            if (i > 2 || i < 0)
+                throw new IndexOutOfRangeException();
+
+            else
+                variableList[i] = item;
+        }
 
     }
 }
