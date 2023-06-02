ac27df3cbcaa90923d456bd566dd371c19a84656
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-06-02 13:09:22 +0100

    Exercise 1
    Replaced class Program

--- HowManyOfThisType/Program.cs
+++ HowManyOfThisType/Program.cs
@@ -1,31 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace HowManyOfThisType
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private const double x = 1.23;
+
+        private static void Main()
         {
-            List<object> objectList = new List<object>();
+            Random rng = new Random();
+            ICollection<object> items = new List<object>();
 
-            int a = 42;
-            string b = "wow";
-            float c = 8f;
-            string d = "Pingas";
-            int e = 69;
+            items.Add(1);                    // int
+            items.Add(1.3);                  // double
+            items.Add(-0.5f);                // float
+            items.Add("hello");              // string
+            items.Add(true);                 // bool
+            items.Add(false);                // bool
+            items.Add(rng.NextDouble() > x); // bool
+            items.Add(Math.PI);              // double
+            items.Add(Math.E);               // double
+            items.Add(rng.NextDouble());     // double
+            items.Add(rng.Next());           // int
 
-            objectList.Add(a);
-            objectList.Add(b);
-            objectList.Add(c);
-            objectList.Add(d);
-            objectList.Add(e);
+            Console.WriteLine($"int   : {Checker.HowManyOfType<int>(items)}");
+            Console.WriteLine($"float : {Checker.HowManyOfType<float>(items)}");
+            Console.WriteLine($"double: {Checker.HowManyOfType<double>(items)}");
+            Console.WriteLine($"bool  : {Checker.HowManyOfType<bool>(items)}");
+            Console.WriteLine($"string: {Checker.HowManyOfType<string>(items)}");
+            Console.WriteLine($"byte  : {Checker.HowManyOfType<byte>(items)}");
 
-            Console.WriteLine(Checker.HowManyOfType<string>(objectList));
-            Console.WriteLine(Checker.HowManyOfType<float>(objectList));
-            Console.WriteLine(Checker.HowManyOfType<int>(objectList));
+            // Expected output
+            // int   : 2
+            // float : 1
+            // double: 4
+            // bool  : 3
+            // string: 1
+            // byte  : 0
         }
     }
 }
