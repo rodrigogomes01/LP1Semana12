24551568594278709eb69f546807440781ccde1c
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-06-02 14:30:31 +0100

    Exercise 5
    Placed the classes from the previous exercise

---
+++ JustLikeACollection/Guarda3.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JustLikeACollection
+{
+    public class Guarda3<T> : IEnumerable<T>
+    {
+        private T v1, v2, v3;
+        private List<T> variableList = new List<T>();
+        public Guarda3()
+        {
+            v1 = default;
+            v2 = default;
+            v3 = default;
+            variableList.Add(v1);
+            variableList.Add(v2);
+            variableList.Add(v3);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return variableList.GetEnumerator();
+        }
+
+        
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public T GetItem(int i)
+        {
+            return variableList[i];
+        }
+        public void SetItem(int i, T item)
+        {
+            if (i>2 || i<0)
+                throw new IndexOutOfRangeException();
+                
+            else
+                variableList[i] = item;
+        }
+
+    }
+}
--- JustLikeACollection/Program.cs
+++ JustLikeACollection/Program.cs
@@ -2,11 +2,40 @@
 
 namespace JustLikeACollection
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Guarda3<string> g3s = new Guarda3<string>();
+            Guarda3<float> g3f = new Guarda3<float>();
+
+            g3s.SetItem(0, "Olá");
+            g3s.SetItem(2, "Adeus");
+
+            g3f.SetItem(2, 54.6f);
+
+            Console.WriteLine("Strings:");
+            foreach (string s in g3s)
+            {
+                Console.WriteLine($"    {s}");
+            }
+
+            Console.WriteLine("Floats:");
+            foreach (float f in g3f)
+            {
+                Console.WriteLine($"    {f}");
+            }
+
+            // Expected output
+
+            // Strings:
+            //     Olá
+            //
+            //     Adeus
+            // Floats:
+            //     0
+            //     0
+            //     54.6
         }
     }
 }
