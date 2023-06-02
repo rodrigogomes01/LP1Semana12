aa00d5eb40e015de0c707c47bfc7f0d8a8cf4f68
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-06-02 13:13:05 +0100

    Exercise 2
    Created project TheyCameBefore

---
+++ TheyCameBefore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheyCameBefore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
