ff5fae7f1c33e185d9bfb399cd15a54dcafee24e
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-06-02 14:45:36 +0100

    Exercise 6
    Created project WriteStuff1

---
+++ WriteStuff1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WriteStuff1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
