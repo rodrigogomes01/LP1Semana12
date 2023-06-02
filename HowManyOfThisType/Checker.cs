bd3a13d621481de3a1230c6d01b5bb740c56a050
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-06-02 13:00:38 +0100

    Exercise 1
    Added static method

--- HowManyOfThisType/Checker.cs
+++ HowManyOfThisType/Checker.cs
@@ -7,6 +7,17 @@ namespace HowManyOfThisType
 {
     public static class Checker
     {
-
+        public static int HowManyOfType<T>(IEnumerable<object> items)
+        {
+            int i = 0;
+            foreach (object o in items)
+            {
+                if (o is T)
+                {
+                    i++;
+                }
+            }
+            return i;
+        }
     }
 }
