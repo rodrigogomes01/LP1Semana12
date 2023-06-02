53a3d0257de3618e0a25093e47ecbfb823e5c275
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-06-02 14:25:50 +0100

    Exercise 5
    Added project JustLikeACollection

---
+++ JustLikeACollection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JustLikeACollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
