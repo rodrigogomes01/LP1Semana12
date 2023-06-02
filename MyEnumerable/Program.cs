70a70e42a3edaa70c7f9e2cd881268ce0755e9f8
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-06-02 14:03:40 +0100

    Exercise 4
    Created project MyEnumerable

---
+++ MyEnumerable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
