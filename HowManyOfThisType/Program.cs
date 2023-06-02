a7b53d1c4aa77581ab1a8c845c7c6bcc8569c981
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-06-02 13:03:59 +0100

    Exercise 1
    Added code testing in Main

--- HowManyOfThisType/Program.cs
+++ HowManyOfThisType/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace HowManyOfThisType
 {
@@ -6,7 +9,23 @@ namespace HowManyOfThisType
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<object> objectList = new List<object>();
+
+            int a = 42;
+            string b = "wow";
+            float c = 8f;
+            string d = "Pingas";
+            int e = 69;
+
+            objectList.Add(a);
+            objectList.Add(b);
+            objectList.Add(c);
+            objectList.Add(d);
+            objectList.Add(e);
+
+            Console.WriteLine(Checker.HowManyOfType<string>(objectList));
+            Console.WriteLine(Checker.HowManyOfType<float>(objectList));
+            Console.WriteLine(Checker.HowManyOfType<int>(objectList));
         }
     }
 }
