67e8a1114d816d52ce432cbf8e7b0f51dadb865e
rodrigogomes01 <rodrigogomes091997@gmail.com>
2023-06-02 14:17:28 +0100

    Exercise 4
    Implemented IEnumerable<T> in Guarda3

--- MyEnumerable/Guarda3.cs
+++ MyEnumerable/Guarda3.cs
@@ -1,24 +1,33 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace MyEnumerable
 {
-    public class Guarda3<T>
+    public class Guarda3<T> : IEnumerable<T>
     {
-        private List<T> variableList = new List<T>();
         private T v1, v2, v3;
-
+        private List<T> variableList = new List<T>();
         public Guarda3()
         {
             v1 = default;
             v2 = default;
             v3 = default;
-
             variableList.Add(v1);
             variableList.Add(v2);
             variableList.Add(v3);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return variableList.GetEnumerator();
+        }
+
+        
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
 
         public T GetItem(int i)
@@ -27,10 +36,9 @@ namespace MyEnumerable
         }
         public void SetItem(int i, T item)
         {
-
-            if (i > 2 || i < 0)
+            if (i>2 || i<0)
                 throw new IndexOutOfRangeException();
-
+                
             else
                 variableList[i] = item;
         }
